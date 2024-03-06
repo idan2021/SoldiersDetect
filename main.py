4f67583a6d6e448b1aa205a79efbeea177a4e41a
idan <idandaar2020@gmail.com>
2024-03-06 11:33:12 +0200

    Pics library (12 soldiers and 6 terrorists) has added to the project. Main is updated accordingly.

--- main.py
+++ main.py
@@ -1,5 +1,21 @@
+import face_recognition
+import os, os.path
+
+
 def main():
-    print("Hello World!")
+    listPicsSoliders=list()
+    listPicsTerrorists=list()
+    lengthOfSoldDir = len([name for name in os.listdir('PICS\\soldiers') if (name.startswith("soldier"))])
+    lengthOfTerDir = len([name for name in os.listdir('PICS\\terrorists') if (name.startswith("terrorist"))])
+    for i in range(0, lengthOfSoldDir-1):
+        soldierPath="PICS\\soldiers\\soldier" + str((i + 1)) + ".jpg"
+        listPicsSoliders.append(face_recognition.load_image_file(soldierPath))
+    for i in range(0,lengthOfTerDir-1):
+        terroristPath="PICS\\terrorists\\terrorist" + str((i + 1)) + ".jpg"
+        listPicsTerrorists.append(face_recognition.load_image_file(terroristPath))
+
+    print("All OK")
+
 
 
 if __name__ == '__main__':
