40c0446ed358a2c01e915b5af1a6936b8545ab6e
idan <idandaar2020@gmail.com>
2024-03-27 18:16:38 +0200

    Added new design for login page and popup can add new comments

--- login.py
+++ login.py
@@ -25,20 +25,12 @@ def login():
     if username in user_passwords.keys():
         print(user_passwords," ",user_passwords.get(username))
         if int(user_passwords.get(username)) == int(password):        # Authentication successful, render map.html
-            #map()
             return render_template('map.html')
         else:
-            #return render_template('map.html')
             # Authentication failed, return error message
             return jsonify({'success': False, 'message': 'Invalid credentials'})
     else:
         print("theres no username")
-
-
-@app.route('/map.html', methods=['GET'])
-def map():
-    # This route is for rendering the map.html template, it doesn't need any logic here
-    return render_template('map.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
